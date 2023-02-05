e2d810430ac3ec14466e34f0b993e7ee1f131eff
happyyumeno <happythousandsun@gmail.com>
2023-02-05 17:02:43 +0900

    message

---
+++ pages/🐰うさぎのページ.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.markdown(
+'<p><span style = "background-color:#ffb6c1;color:#ffffff;font-size: 30px;font-family:Kosugi Maru;"> ★　うさぎのページ　★ </span></p>',
+unsafe_allow_html=True)
+
+path="./main/もち.jpg"
+st.image(f"{path}", use_column_width=True)
+
