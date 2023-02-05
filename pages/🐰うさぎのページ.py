18418b350bd00973dd04522c95b6bb372d6d7e0e
happyyumeno <happythousandsun@gmail.com>
2023-02-05 17:11:36 +0900

    message

--- pages/🐰うさぎのページ.py
+++ pages/🐰うさぎのページ.py
@@ -8,6 +8,6 @@ st.markdown(
 unsafe_allow_html=True)
 
 # path=".//main/もち.jpg"
-path=https://github.com/happyyumeno/coderdojotckw/blob/master/main/%E3%82%82%E3%81%A1.JPG?raw=true
+path="https://github.com/happyyumeno/coderdojotckw/blob/master/main/%E3%82%82%E3%81%A1.JPG?raw=true"
 st.image(f"{path}", use_column_width=True)
 
