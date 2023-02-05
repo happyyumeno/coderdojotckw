06ebbfa4762de3cf18fa2b9cbe263e55a87e2619
happyyumeno <happythousandsun@gmail.com>
2023-02-05 17:10:26 +0900

    message

--- pages/🐰うさぎのページ.py
+++ pages/🐰うさぎのページ.py
@@ -7,6 +7,7 @@ st.markdown(
 '<p><span style = "background-color:#ffb6c1;color:#ffffff;font-size: 30px;font-family:Kosugi Maru;"> ★　うさぎのページ　★ </span></p>',
 unsafe_allow_html=True)
 
-path=".//main/もち.jpg"
+# path=".//main/もち.jpg"
+path=https://github.com/happyyumeno/coderdojotckw/blob/master/main/%E3%82%82%E3%81%A1.JPG?raw=true
 st.image(f"{path}", use_column_width=True)
 
